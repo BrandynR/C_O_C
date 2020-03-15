2318fdcb22fd0263522ae86b69c7ab9dc5c499af
BrandynR <brandynr@github.com>
2020-03-14 22:35:23 -0400

    Added checkboard transition

--- src/scenes/Playscene.js
+++ src/scenes/Playscene.js
@@ -23,6 +23,7 @@ export class PlayScene extends Phaser.Scene {
         this.load.image("air", "./assets/air_card.png")
         this.load.image("fire", "./assets/fire_card.png")
         this.load.image("water", "./assets/water_card.png")
+        this.load.image('block', 'assets/tiny2.png');
 
         this.load.once("loaderror", function(file) {
             console.log(file)
@@ -30,8 +31,6 @@ export class PlayScene extends Phaser.Scene {
         console.log("You've made it to preload")
     }
     create() {
-        //this.add.image(this.game.renderer.width / 5, this.game.renderer.height / 4, "wiz2").setDepth(1);
-        //this.add.image(this.game.renderer.width / 4, this.game.renderer.height / 4, "wiz1").setDepth(1);
         this.add.image(0, 0, "terrain").setOrigin(0).setDepth(0);
         let Player1 = this.add.sprite(225, 300, "wiz1").setDepth(1);
         Player1.setScale(0.75);
@@ -54,14 +53,52 @@ export class PlayScene extends Phaser.Scene {
         let Earth2 = this.add.sprite(910, 575, "earth").setDepth(1);
         Earth2.setScale(0.70);
 
-        // sprite
-        /*        capguy = this.add.sprite(0, 400, 'Wizard1', "PNG/wizard/5_ATTACK_004.png");
-                capguy.setScale(0.5, 0.5);
+        //Checkerboard transition
+        var blocks = this.add.group({ key: 'block', repeat: 300 }).setDepth(1);
+        //blocks.setScale(1 / 15);
 
-                // animation
-                var frameNames = this.anims.generateFrameNames('Wizard1', { start: 1, end: 8, zeroPad: 4, prefix: 'PNG/wizard/', suffix: '.png' });
-                this.anims.create({ key: 'attack', frames: frameNames, frameRate: 10, repeat: -1 });
-                Player1.anims.play('attack');
-        */
+        Phaser.Actions.GridAlign(blocks.getChildren(), {
+            width: 21,
+            cellWidth: 60,
+            cellHeight: 60,
+            x: 0,
+            y: 0
+        });
+
+        var _this = this;
+
+        var i = 0;
+
+        blocks.children.iterate(function(child) {
+
+            _this.tweens.add({
+                targets: child,
+                scaleX: 0,
+                scaleY: 0,
+                alpha: 0,
+                y: '+=64',
+                angle: 180,
+                ease: 'Power3',
+                duration: 1000,
+                delay: 1000 + (i * 100)
+            });
+
+            i++;
+
+            //  Change the value 32 for different results
+            if (i % 16 === 0) {
+                i = 0;
+            }
+
+            // sprite
+            /*        capguy = this.add.sprite(0, 400, 'Wizard1', "PNG/wizard/5_ATTACK_004.png");
+                    capguy.setScale(0.5, 0.5);
+
+                    // animation
+                    var frameNames = this.anims.generateFrameNames('Wizard1', { start: 1, end: 8, zeroPad: 4, prefix: 'PNG/wizard/', suffix: '.png' });
+                    this.anims.create({ key: 'attack', frames: frameNames, frameRate: 10, repeat: -1 });
+                    Player1.anims.play('attack');
+            */
+        });
     }
 }
